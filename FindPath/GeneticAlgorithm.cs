97c30feb87b5aada900cc0e4a647e76df3b59d9d
Awni <Wintrygamer@gmail.com>
2020-11-13 12:28:21 +0100

    Create generic genetic algorithm and genome class
    - Create genome class that contains fitness and gene value
    - Create genetic Algorithm class with a population of 20 genomes. The class will be used to run the algorithm.

---
+++ FindPath/GeneticAlgorithm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FindPath
+{
+    class GeneticAlgorithm<T>
+    {
+        Genome<T>[] population;
+        const int populationSize = 20;
+
+        public GeneticAlgorithm()
+        {
+            
+        }
+
+    }
+}
