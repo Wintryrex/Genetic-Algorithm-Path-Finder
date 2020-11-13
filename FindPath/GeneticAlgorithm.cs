041522cf1049db5149d68aa7ca4862acefc365d3
Awni <Wintrygamer@gmail.com>
2020-11-13 20:14:06 +0100

    Create selection
    Create selection (roulette wheel selection)

--- FindPath/GeneticAlgorithm.cs
+++ FindPath/GeneticAlgorithm.cs
@@ -8,11 +8,15 @@ namespace FindPath
     {
         Func<T[], float> evalFitness;
         Genome<T>[] population;
+        Random rnd;
         const int populationSize = 20;
 
-        public GeneticAlgorithm(Func<T[]> randomSolution, Func<T[], float> evalFitness)
+        float fitnessSum;
+
+        public GeneticAlgorithm(Func<T[]> randomSolution, Func<T[], float> evalFitness, Random rnd)
         {
             this.evalFitness = evalFitness;
+            this.rnd = rnd;
             CreatePopulation(randomSolution);
             SetFitness();
         }
@@ -30,13 +34,36 @@ namespace FindPath
 
         private void SetFitness()
         {
+            fitnessSum = 0;
+
             for (int i = 0; i < population.Length; ++i)
             {
-                population[i].Fitness = evalFitness(population[i].Genes);
+                float fitness = evalFitness(population[i].Genes); 
+                population[i].Fitness = fitness;
+                fitnessSum += fitness;
             }
 
             Array.Sort(population, (x, y) => x.Fitness.CompareTo(y.Fitness));
         }
+
+        private Genome<T> Selection() 
+        {
+            double random = rnd.NextDouble() * fitnessSum;
+
+            for (int i = 0; i < population.Length; ++i)
+            {
+                if (random < population[i].Fitness)
+                {
+                    return population[i];
+                }
+
+                random -= population[i].Fitness;
+            }
+
+            return null;
+        }
+
+
 
     }
 }
